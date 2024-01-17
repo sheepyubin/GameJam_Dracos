679e80cb2b631ec7dbb433075ab8ecb70c3278f8
sheepyubin <yangyubinstory@naver.con>
2024-01-17 17:40:56 +0900

    .
    .

---
+++ Is-the-order-a-Yubin/Assets/Script/Yubin/Game/Event.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    public Timer timer;
+
+    private void Update()
+    {
+        if (timer != null)
+        {
+            if (timer.timerTime >= 60)
+            {
+
+            }
+        }
+    }
+
+    public void Evolution()
+    {
+
+    }
+}
