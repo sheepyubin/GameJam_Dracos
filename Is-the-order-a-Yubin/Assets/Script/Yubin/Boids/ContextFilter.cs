d21a3276cb25848b5e5b37add7dee06c600a738e
sheepyubin <yangyubinstory@naver.con>
2024-01-18 10:41:41 +0900

    .
    .

--- Is-the-order-a-Yubin/Assets/Script/Yubin/Boids/ContextFilter.cs
+++ Is-the-order-a-Yubin/Assets/Script/Yubin/Boids/ContextFilter.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 // ContextFilter 추상 클래스
 public abstract class ContextFilter : ScriptableObject
 {
-    // Filter() 각 개체가 어떻게 움직일지 계산하는 추상 메서드
-    //Parameters:
-    // - agent: 현재 Boids 객체
-    // - context: 주변 개체의 Transform 목록
+    // Filter 메서드: BoidsAgent와 근처 개체의 리스트를 받아 필터링된 결과를 반환
+    // Parameters:
+    //   - agent: Boids 개체
+    //   - original: 근처 개체 목록
     // Returns:
-    // - 다음 프레임에 이동할 방향을 나타내는 벡터
+    //   - 필터링된 근처 개체 목록
     public abstract List<Transform> Filter(BoidsAgent agent, List<Transform> original);
 }
