3ab6770394a5e1c728f636a034e21e7515f81605
sheepyubin <yangyubinstory@naver.con>
2024-01-18 12:10:14 +0900

    주석
    .

--- Is-the-order-a-Yubin/Assets/Script/Yubin/Boids/Filter Scripts/PhysicsLayerFilter.cs
+++ Is-the-order-a-Yubin/Assets/Script/Yubin/Boids/Filter Scripts/PhysicsLayerFilter.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 레이어 마스크로 근처 오브젝트를 필터링
 [CreateAssetMenu(menuName = "Boids/Filter/Physics Layer")]
 public class PhysicsLayerFilter : ContextFilter
 {
+    // 레이어 마스크
     public LayerMask mask;
 
+    // Filter() 메서드: BoidsAgent와 근처 오브젝트를 목록을 필터링
+    // Parameters:
+    //   - agent: Boids 개체
+    //   - original: 근처 오브젝트를 목록
+    // Returns:
+    //   - 필터링된 근처 오브젝트를 목록
     public override List<Transform> Filter(BoidsAgent agent, List<Transform> original)
     {
+        // 필터링된 오브젝트를 담을 리스트
         List<Transform> filtered = new List<Transform>();
-        foreach (Transform i in original)
+
+        // 근처 오브젝트를 목록을 순회하면서 개체를 찾아 필터링
+        foreach (Transform objTransform in original)
         {
-            if (mask == (mask | (1 << i.gameObject.layer)))
+            if (mask == (mask | (1 << objTransform.gameObject.layer)))
             {
-                filtered.Add(i);
+                // 필터링된 리스트에 추가
+                filtered.Add(objTransform);
             }
         }
+
+        // 오브젝트를 목록 반환
         return filtered;
     }
 }
--- Is-the-order-a-Yubin/Assets/Script/Yubin/Boids/Filter Scripts/SameBoidsFilter.cs
+++ Is-the-order-a-Yubin/Assets/Script/Yubin/Boids/Filter Scripts/SameBoidsFilter.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// SameBoidsFilter: 같은 개체를 필터링하는 ScriptableObject 클래스
 [CreateAssetMenu(menuName = "Boids/Filter/Same Boids")]
 public class SameBoidsFilter : ContextFilter
 {
+    // Filter 메서드 구현: 같은 개체를 필터링
+    // Parameters:
+    //   - agent: Boids 개체
+    //   - original: 근처 개체 목록
+    // Returns:
+    //   - 필터링된 근처 개체 목록
     public override List<Transform> Filter(BoidsAgent agent, List<Transform> original)
     {
+        // 필터링된 개체를 담을 리스트
         List<Transform> filtered = new List<Transform>();
-        foreach (Transform item in original)
+
+        // 근처 개체 목록을 순회하면서 개체를 찾아 필터링
+        foreach (Transform objTransform in original)
         {
-            BoidsAgent itemAgent = item.GetComponent<BoidsAgent>();
+            BoidsAgent itemAgent = objTransform.GetComponent<BoidsAgent>();
+
+            // 필터링된 리스트에 추가
             if (itemAgent != null && itemAgent.AgentBoid == agent.AgentBoid)
             {
-                filtered.Add(item);
+                filtered.Add(objTransform);
             }
         }
+
+        // 필터링된 인접 오브젝트 목록 반환
         return filtered;
     }
 }
