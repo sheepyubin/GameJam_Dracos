4410d55c22086758671238d42e28d1b70fd84e2c
Yeeeen00 <king0000427@gmail.com>
2024-01-18 12:31:49 +0900

    BGM Add

--- Is-the-order-a-Yubin/Assets/Audio/SetVolume.cs
+++ Is-the-order-a-Yubin/Assets/Audio/SetVolume.cs
@@ -9,16 +9,29 @@ public class SetVolume : MonoBehaviour
     // Start is called before the first frame update
 
     public AudioMixer mixer;
-    public Slider slider;
+    public Slider MasterSlider;
+    public Slider BackSlider;
+    public Slider EffectSlider;
     
-    void Start()
+    void Awake()
     {
-        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        MasterSlider.onValueChanged.AddListener(SetMasterVolume);
+        BackSlider.onValueChanged.AddListener(SetBackVolume);
+        EffectSlider.onValueChanged.AddListener(SetEffectVolume);
     }
 
-    public void SetLevel(float sliderValue)
+    public void SetMasterVolume(float volume)
     {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
-        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+        mixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetBackVolume(float volume)
+    {
+        mixer.SetFloat("Back", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        mixer.SetFloat("Effect", Mathf.Log10(volume) * 20);
     }
 }
